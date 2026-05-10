b51860082363f3d857fb0a4cc894890dc7d1aeb4
Quentin <quentin.prunet@epita.fr>
2026-05-10 19:17:43 +0200

    feat: tests for order class

--- tests/test_order.cc
+++ tests/test_order.cc
@@ -6,4 +6,9 @@
 TEST(CoreClasses, Order) {
     Order o = Order{1, 1, 10, 30, OrderType::BUY, "SPY"};
     EXPECT_EQ(o.getOrderId(), 1);
+    EXPECT_EQ(o.getCustomerId(), 1);
+    EXPECT_EQ(o.getAmount(), 10);
+    EXPECT_EQ(o.getPricePerUnit(), 30);
+    EXPECT_EQ(o.getType(), OrderType::BUY);
+    EXPECT_EQ(o.getInstrument(), "SPY");
 }
