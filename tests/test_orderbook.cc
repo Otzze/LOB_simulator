ceff2aa70d173858af04eddfa91543007c4b8295
Quentin <quentin.prunet@hexamind.ai>
2026-06-05 12:16:00 +0200

    feat: testing Order and OrderBook classes

---
+++ tests/test_orderbook.cc
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "lob/order_type.hh"
+#include "lob/order.hh"
+#include "lob/orderbook.hh"
+
+TEST(CoreClasses, OrderBook) {
+    OrderBook OB = OrderBook("SPY");
+    EXPECT_EQ(OB.GetInstrumentName(), "SPY");
+}
+
+TEST(CoreClasses, OrderBookAdd) {
+    OrderBook OB = OrderBook("SPY");
+    Order o = Order{1, 1, 10, 30, OrderType::BUY, "SPY"};
+    OB.InsertOrder(o);
+}
